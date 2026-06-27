f89b531e36e84e05bcb74942c538b79e09765d3b
DineshBoppudi <dineshboppudi13@gmail.com>
2026-06-27 14:03:41 +0530

    Added test route

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.post("/login", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Backend Running Successfully");
-});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
